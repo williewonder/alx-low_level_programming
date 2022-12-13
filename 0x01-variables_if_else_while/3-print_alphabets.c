c098a8e935de0c090896ce1fdcd9fe9b6b7ba0a6
williewonder <damilarewilliams69@gmail.com>
2022-12-12 18:28:42 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphebet;
+
+for (alphebet = 'a'; alphebet <= 'z'; alphebet++)
+{
+putchar(alphebet);
+}
+
+for (alphebet = 'A'; alphebet <= 'Z'; alphebet++)
+{
+putchar(alphebet);
+}
+putchar('\n');
+return (0);
+}
