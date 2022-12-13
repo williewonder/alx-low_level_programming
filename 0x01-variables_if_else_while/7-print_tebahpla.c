3cbc385174fe1b7dac09748c4a2fca8dfb9c695a
williewonder <damilarewilliams69@gmail.com>
2022-12-12 18:42:14 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphebet;
+
+	for (alphebet = 'z'; alphebet >= 'a'; alphebet--)
+		putchar(alphebet);
+	putchar('\n');
+
+	return (0);
+}
