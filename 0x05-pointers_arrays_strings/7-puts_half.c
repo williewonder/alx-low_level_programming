471a4499a71d0252d26ae0afa6278c9065f0bf11
williewonder <damilarewilliams69@gmail.com>
2022-12-20 09:04:24 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string character
+ * Return: the second half of the string
+ */
+void puts_half(char *str)
+{
+	int count = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (( count - 1) / 2)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			i = count % 2 == 1;
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+
